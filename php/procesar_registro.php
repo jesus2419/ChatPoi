41ce58386597649c963cb99af34965c8c75c89ab
jesus2419 <102651007+jesus2419@users.noreply.github.com>
2023-10-15 21:00:36 -0600

    Update procesar_registro.php

--- php/procesar_registro.php
+++ php/procesar_registro.php
@@ -15,16 +15,26 @@ $uname = $conn->real_escape_string($uname);
 $telefono = $conn->real_escape_string($telefono);
 $password = $conn->real_escape_string($password);
 
-// Crea la consulta SQL para insertar en la tabla de usuarios (cambia 'tu_tabla' al nombre de tu tabla)
-$sql = "INSERT INTO usuarios (Nombre, Apellidos, NombreUsuario, UsuarioPassword, telefono) VALUES ('$nombre', '$apellidos', '$uname', '$password', '$telefono')";
+// Verifica si el nombre de usuario ya existe
+$check_query = "SELECT * FROM usuarios WHERE NombreUsuario='$uname'";
+$result = $conn->query($check_query);
 
-// Ejecuta la consulta y verifica si fue exitosa
-if ($conn->query($sql) === TRUE) {
-     // Redirige a otro HTML después de un segundo (cambia 'otro_html.html' al nombre de tu HTML de destino)
-     header("Location: chat.php?username=$uname");
-    //echo "Registro exitoso";
+if ($result->num_rows > 0) {
+    // El nombre de usuario ya existe, puedes mostrar un mensaje de error
+    echo "El nombre de usuario ya está en uso. Por favor, elige otro nombre de usuario.";
 } else {
-    echo "Error al registrar el usuario: " . $conn->error;
+    // El nombre de usuario no existe, puedes proceder con la inserción
+    // Crea la consulta SQL para insertar en la tabla de usuarios (cambia 'tu_tabla' al nombre de tu tabla)
+    $sql = "INSERT INTO usuarios (Nombre, Apellidos, NombreUsuario, UsuarioPassword, telefono) VALUES ('$nombre', '$apellidos', '$uname', '$password', '$telefono')";
+
+    // Ejecuta la consulta y verifica si fue exitosa
+    if ($conn->query($sql) === TRUE) {
+        // Redirige a otro HTML después de un segundo (cambia 'otro_html.html' al nombre de tu HTML de destino)
+        header("Location: chat.php?username=$uname");
+        //echo "Registro exitoso";
+    } else {
+        echo "Error al registrar el usuario: " . $conn->error;
+    }
 }
 
 // Cierra la conexión
